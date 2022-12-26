bf1e665d689fa460654cb04a628b2a5f8d853ee4
Divyesh-1234 <121417866+Divyesh-1234@users.noreply.github.com>
2022-12-26 11:13:40 +0530

    Add files via upload

---
+++ assigment/module1/module 1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("\nkotadiya Divyesh");
+    printf ("\n03/08/1999");
+    printf("\n23");
+    printf("\n6,new kedarnath socitey,\nkothariya main road rajkot.");
+    
+    return 0;
+}
