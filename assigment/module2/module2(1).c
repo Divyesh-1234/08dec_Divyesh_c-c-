0f22b97df735797928b3ef7c7161bfe1172b414f
Divyesh-1234 <121417866+Divyesh-1234@users.noreply.github.com>
2022-12-26 11:15:41 +0530

    Add files via upload

---
+++ assigment/module2/module2(1).c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int a,b,number;
+    printf("Enter the number of:");
+    scanf("%d",&number);
+    a=number*number;
+    printf("\n square:%d",a);
+    b=number*number*number;
+    printf("\n cube:%d",b);
+    
+    //printf("");
+
+    return 0;
+}
