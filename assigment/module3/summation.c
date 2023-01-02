4ed4156769602a0d061219eb046daa5e2e944631
Divyesh-1234 <121417866+Divyesh-1234@users.noreply.github.com>
2023-01-02 11:31:01 +0530

    Add files via upload

---
+++ assigment/module3/summation.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,d,g;
+    printf("enter a value:");
+    scanf("%d",&a);
+    printf("enter b value:");
+    scanf("%d",&b);
+    printf("enter c value:");
+    scanf("%d",&c);
+    printf("enter d Value:");
+    scanf("%d",&d);
+    g=a+b+c+d;
+    printf("summation:%d",g);
+    return 0;
+    
+}
