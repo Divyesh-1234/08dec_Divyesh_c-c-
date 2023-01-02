0e2deb98d0d0cece375d76be234cf05cec6eac83
Divyesh-1234 <121417866+Divyesh-1234@users.noreply.github.com>
2023-01-02 11:15:03 +0530

    Add files via upload

---
+++ assigment/module3/leap year.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	int year;
+	printf("Enter the year");
+	scanf("%d",&year);
+	if(year%4==0)
+	{
+		printf("leap year");
+		
+	}
+	else 
+	{
+		printf("not Leap year");
+		
+	}
+	return 0;
+	
+}
