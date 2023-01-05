cb124433c7686ac728cb581039926f095eabf826
Divyesh-1234 <121417866+Divyesh-1234@users.noreply.github.com>
2023-01-05 14:35:19 +0530

    Add files via upload

---
+++ task/Month, year.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+
+int main()
+{
+    float month, year;
+    printf("Enter the number of month:");
+    scanf("%f",&month);
+    year=month/12;
+    printf("Number of the month is:%f",year);
+    
+    return 0;
+}
