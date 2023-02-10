49e0d95267fd93b79b0df2695044d990a520369e
Divyesh-1234 <121417866+Divyesh-1234@users.noreply.github.com>
2023-02-10 09:55:16 +0530

    Add files via upload

---
+++ task/Do while loop.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int d=1;
+   
+    do
+    {
+        cout<< d << endl, d++;
+        
+       
+    }
+    while(d<=5);
+    return 0;
+}
