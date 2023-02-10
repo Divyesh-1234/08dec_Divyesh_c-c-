cbfc5fdd6eda2df4dde060fa7f1910911684fe8d
Divyesh-1234 <121417866+Divyesh-1234@users.noreply.github.com>
2023-02-10 09:38:51 +0530

    Add files via upload

---
+++ assigment/module3/two number using pointer.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int *d,*k;
+	int num1,num2,addition;
+	printf("Enter the First number:");
+	scanf("%d",&num1);
+	printf("Enter the Second number:");
+	scanf("%d",&num2);
+	d=&num1;
+	k=&num2;
+	addition=*d+*k;
+	printf("Two number addition:%d",addition);
+	return 0;
+}
